ecd70457a9bc8acfd52f574bad6459fb5afae962
10BenAgain <benjones511@gmail.com>
2024-07-15 12:36:29 -0600

    More hint typing

--- arduino/rpi/constants.py
+++ arduino/rpi/constants.py
@@ -11,14 +11,14 @@ with open("settings.json") as s:
     rate = settings["rate"]
 
 
-instruct_modes = {
+instruct_modes: dict[str, int] = {
     "RLG RNG": 0,
     "RSE RNG": 1,
     "ACE Seed Checker": 2,
     "Custom Seed Checker": 3
 }
 
-button_map = {
+button_map: dict[str, int]  = {
     "A": 2,
     "B": 3,
     "X": 4,
@@ -34,12 +34,12 @@ button_map = {
     "Power": 14
 }
 
-audio_mode = {
+audio_mode: dict[str, int]  = {
     "Mono": 0,
     "Stereo": 1
 }
 
-shoulder_mode = {
+shoulder_mode: dict[str, int]  = {
     "L=A": 0,
     "HELP": 1,
     "LR": 2
